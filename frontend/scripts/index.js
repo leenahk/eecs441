17e178a7c1372512a1c1473040b43949a772bcb2
Ashvin Pai <avpai@umich.edu>
2024-10-17 19:38:52 -0400

    make request to API when user logs in

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,5 +1,21 @@
 function submitUsername() {
     const username = document.getElementById("usernameInput").value;
+    fetch('http://localhost:5000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "username": "user1" })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+
     sessionStorage.setItem("username", username);
     window.location.href = "main.html";
 }
